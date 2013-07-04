3437b06635c82515f006e085b9305b66a5158bb8
Magnus Lund <mgnusl@gmail.com>
2013-07-04 04:35:18 +0200

    its working

--- src/com/example/currencyconverter/MainActivity.java
+++ src/com/example/currencyconverter/MainActivity.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.apache.http.HttpEntity;
@@ -21,25 +22,84 @@ import org.json.JSONObject;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	private static final String TAG = "APP";
+	
+	private Map<String, Double> currencyRates;
 
-	TextView tekst1, tekst2;
+	private TextView resultTV;
+	private EditText amountET;
+	private Spinner fromSpinner, toSpinner;
+	private Button calculateButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		tekst1 = (TextView) findViewById(R.id.tekst1);
-		tekst2 = (TextView) findViewById(R.id.tekst2);
 		
+		fromSpinner = (Spinner)findViewById(R.id.fromCurrencySpinner);
+		toSpinner = (Spinner)findViewById(R.id.toCurrencySpinner);
+		calculateButton = (Button)findViewById(R.id.calculateButton);
+		resultTV = (TextView)findViewById(R.id.resultTV);
+		amountET = (EditText)findViewById(R.id.amountET);
+		
+		calculateButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				calculateCurrency();
+
+			}
+		});
+
 		new DownloadJSON().execute();
+		
+		
 	
+	}
+	
+	@SuppressWarnings("rawtypes")
+	private void setSpinnerAdapter() {
+		
+		// iterates through the hashmap, adding the keys to an arraylist
+		ArrayList<String> list = new ArrayList<String>();
+		Iterator it = currencyRates.entrySet().iterator();
+	    while (it.hasNext()) {
+	        Map.Entry pairs = (Map.Entry)it.next();
+	        list.add((String)pairs.getKey());
+	    }
+		
+	    // adding the adapter to both spinners
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		fromSpinner.setAdapter(dataAdapter);
+		toSpinner.setAdapter(dataAdapter);
+		
+		
+	}
+	
+	public void calculateCurrency() {
+		
+		String from = (String) fromSpinner.getSelectedItem();
+		String to = (String) toSpinner.getSelectedItem();
+		double amount = Double.parseDouble(amountET.getText().toString());
+
+		// calculates from currency to currency via euro
+		double result = (amount / currencyRates.get(from)) * currencyRates.get(to);
+		
+		resultTV.setText(Double.toString(result));
+		
+		
+		
 	}
 
 	private class DownloadJSON extends AsyncTask<Void, Void, Void> {
@@ -51,7 +111,7 @@ public class MainActivity extends Activity {
 				JSONObject jObject;
 				jObject = new JSONObject(s);
 				
-				Map<String, Double> currencyRates = new HashMap<String, Double>() ;
+				currencyRates = new HashMap<String, Double>() ;
 					
 				JSONObject query = jObject.getJSONObject("query");
 				JSONObject results = query.getJSONObject("results");
@@ -68,7 +128,7 @@ public class MainActivity extends Activity {
 				}
 				
 				for (Map.Entry entry : currencyRates.entrySet()) {
-					Log.d(TAG, ("Key = " + entry.getKey() + ", Value = " + entry.getValue()));
+					//Log.d(TAG, ("Key = " + entry.getKey() + ", Value = " + entry.getValue()));
 		        }
 				
 
@@ -87,7 +147,8 @@ public class MainActivity extends Activity {
 		@Override
 		protected void onPostExecute(Void result) {
 			super.onPostExecute(result);
-			
+			setSpinnerAdapter();
+						
 		}
 
 	}
