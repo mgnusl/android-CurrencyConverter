f5ab3f643bbf38b92046417cdba7568e185d8f98
Magnus Lund <mgnusl@gmail.com>
2013-07-05 03:14:32 +0200

    using currency pairs instead of EUR base

--- src/com/example/currencyconverter/MainActivity.java
+++ src/com/example/currencyconverter/MainActivity.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.text.DecimalFormat;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -15,7 +12,6 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -34,12 +30,13 @@ public class MainActivity extends Activity {
 
 	private static final String TAG = "APP";
 	
-	private Map<String, Double> currencyRates;
-
 	private TextView resultTV;
 	private EditText amountET;
 	private Spinner fromSpinner, toSpinner;
 	private Button calculateButton;
+	
+	private String toCurrency, fromCurrency;
+	private double currencyRate, amount;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -52,54 +49,44 @@ public class MainActivity extends Activity {
 		resultTV = (TextView)findViewById(R.id.resultTV);
 		amountET = (EditText)findViewById(R.id.amountET);
 		
+		setSpinnerAdapter();
+		
+		currencyRate = 0;
+				
 		calculateButton.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				calculateCurrency();
-
 			}
-		});
-
-		new DownloadJSON().execute();
-		
-		
+		});		
 	
 	}
 	
-	@SuppressWarnings("rawtypes")
 	private void setSpinnerAdapter() {
-		
-		// iterates through the hashmap, adding the keys to an arraylist
-		ArrayList<String> list = new ArrayList<String>();
-		Iterator it = currencyRates.entrySet().iterator();
-	    while (it.hasNext()) {
-	        Map.Entry pairs = (Map.Entry)it.next();
-	        list.add((String)pairs.getKey());
-	    }
-		
-	    // adding the adapter to both spinners
-		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+
+		// adding the adapter to both spinners
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, getResources()
+						.getStringArray(R.array.currencies));
 		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		fromSpinner.setAdapter(dataAdapter);
 		toSpinner.setAdapter(dataAdapter);
-		
 		
 	}
 	
 	public void calculateCurrency() {
 		
-		String from = (String) fromSpinner.getSelectedItem();
-		String to = (String) toSpinner.getSelectedItem();
-		double amount = Double.parseDouble(amountET.getText().toString());
+		toCurrency = (String) toSpinner.getSelectedItem();
+		toCurrency = toCurrency.substring(0, Math.min(3, toCurrency.length()));
 
-		// calculates from currency to currency via euro
-		double result = (amount / currencyRates.get(from)) * currencyRates.get(to);
-		
-		resultTV.setText(Double.toString(result));
-		
-		
-		
+		fromCurrency = (String) fromSpinner.getSelectedItem();
+		fromCurrency = fromCurrency.substring(0, Math.min(3, fromCurrency.length()));
+
+		amount = Double.parseDouble(amountET.getText().toString());
+
+		new DownloadJSON().execute();
+							
 	}
 
 	private class DownloadJSON extends AsyncTask<Void, Void, Void> {
@@ -107,36 +94,25 @@ public class MainActivity extends Activity {
 		@Override
 		protected Void doInBackground(Void... params) {
 			try {
-				String s = getJSON("http://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20eurofx.daily&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=");
+				
+				String s = getJSON("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22"
+						+ fromCurrency + toCurrency + "%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=");
 				JSONObject jObject;
 				jObject = new JSONObject(s);
 				
-				currencyRates = new HashMap<String, Double>() ;
-					
 				JSONObject query = jObject.getJSONObject("query");
 				JSONObject results = query.getJSONObject("results");
-				JSONArray cube = results.getJSONArray("Cube");
-				
-				if(cube != null) {
-					int arrayLength = cube.length();
-					for(int i = 0; i < arrayLength; i++) {
-						
-						JSONObject currency = (JSONObject) cube.get(i);
-						currencyRates.put(currency.getString("currency"), Double.parseDouble((currency.getString("rate"))));					
-						
-					}
-				}
-				
-				for (Map.Entry entry : currencyRates.entrySet()) {
-					//Log.d(TAG, ("Key = " + entry.getKey() + ", Value = " + entry.getValue()));
-		        }
-				
+				currencyRate = Double.parseDouble(results.getJSONObject("rate").getString("Rate"));
+							
 
-			} catch (JSONException e) {
+			} 
+			catch (JSONException e) {
 				e.printStackTrace();
-			} catch (ClientProtocolException e) {
+			} 
+			catch (ClientProtocolException e) {
 				e.printStackTrace();
-			} catch (IOException e) {
+			} 
+			catch (IOException e) {
 				e.printStackTrace();
 			}
 
@@ -147,10 +123,18 @@ public class MainActivity extends Activity {
 		@Override
 		protected void onPostExecute(Void result) {
 			super.onPostExecute(result);
-			setSpinnerAdapter();
-						
+			// currencyrate is fetched, continue calculating
+			continueCalculate();
 		}
 
+	}
+	
+	public void continueCalculate() {
+		
+		double result = amount * currencyRate;
+		DecimalFormat df = new DecimalFormat("#.##");
+		resultTV.setText(df.format(result));
+		
 	}
 
 	private String getJSON(String URL) throws ClientProtocolException, IOException {
